ca347527a83430d37d463957085be27e2d38a230
Nathan Ward <nathanward1234@gmail.com>
2021-08-03 16:39:18 -0500

    improved configuration for gusto

--- client-app/src/main/java/org/springframework/security/oauth/samples/web/AuthorizationController.java
+++ client-app/src/main/java/org/springframework/security/oauth/samples/web/AuthorizationController.java
@@ -22,7 +22,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 /**
@@ -31,8 +31,8 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
 @Controller
 public class AuthorizationController {
 
-	@Value("${messages.base-uri}")
-	private String messagesBaseUri;
+	@Value("${gusto-api.base-uri}")
+	private String gustoApiURL;
 
 	@Autowired
 	private WebClient webClient;
@@ -40,41 +40,33 @@ public class AuthorizationController {
 
 	@GetMapping(value = "/authorize", params = "grant_type=authorization_code")
 	public String authorization_code_grant(Model model) {
+	  
 		System.out.println("Wrong function");
-		String[] messages = retrieveMessages("messaging-client-auth-code");
-		model.addAttribute("messages", messages);
+		
+	      ResponseSpec response = this.webClient
+	          .get()
+	          .uri(this.gustoApiURL)
+	          .attributes(clientRegistrationId("gusto-client"))
+	          .retrieve();
+		
+	    System.out.print("Got HTTP Response!");
+	    
+	    System.out.println("Response.toString: " + response.toString());
+	    
+//		model.addAttribute("messages", messages);
+		
 		return "index";
 	}
 
 	@GetMapping("/authorized")		// registered redirect_uri for authorization_code
 	public String authorized(Model model) {
-		System.out.println("redirect uri in authorized function");
-//		String[] messages = retrieveMessages("messaging-client-auth-code");
-//		model.addAttribute("messages", messages);
-		return "login";
+		
+	  System.out.println("redirect uri in authorized function");
+
+	  //		String[] messages = retrieveMessages("messaging-client-auth-code");
+	  //		model.addAttribute("messages", messages);
+
+	  return "login";
 	}
 
-	@GetMapping(value = "/authorize", params = "grant_type=client_credentials")
-	public String client_credentials_grant(Model model) {
-		String[] messages = retrieveMessages("messaging-client-client-creds");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-
-	@PostMapping(value = "/authorize", params = "grant_type=password")
-	public String password_grant(Model model) {
-		String[] messages = retrieveMessages("messaging-client-password");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
-
-	private String[] retrieveMessages(String clientRegistrationId) {
-		return this.webClient
-				.get()
-				.uri(this.messagesBaseUri)
-				.attributes(clientRegistrationId(clientRegistrationId))
-				.retrieve()
-				.bodyToMono(String[].class)
-				.block();
-	}
 }
